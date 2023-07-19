76cd91dd4320a02a6e69e3cc618e4a690e8eb736
cater20 <onyangoketta@gmail.com>
2023-07-19 04:31:16 -0700

    alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,15 +3,16 @@
 /**
  * print_alphabet - utilizes on the _putchar function to print
  * prints alphabets in lowercase
- * Return :Always (0) success 
- *            
+ * Return :Always (0) successe
+ *e
 */
 
 void print_alphabet(void)
 {
-	int ch;
 
-	for (ch = 'a'; ch <= 'z'; ++ch)
-		_putchar(ch);
-	_putchar('\n');
+int ch;
+
+for (ch = 'a'; ch <= 'z'; ++ch)
+_putchar(ch);
+_putchar('\n');
 }
